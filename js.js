ca19da24e14c62b63e5a1774a524dcf87c283543
Matheus Carvalho <matheusscarvlh@gmail.com>
2024-08-25 14:43:44 -0300

    att

---
+++ js.js
@@ -0,0 +1,3 @@
+window.alert('Oi Olhoes, queria te dizer que estou alguns meses preparando esse presente pra você. Espero que goste!')
+
+window.alert('Aqui eu carrego o nosso PASSADO, por todo o nosso FUTURO! <3')
