8fe12dac2266d86d3d7a1e0ede3579b60812521d
Matheus Carvalho <matheusscarvlh@gmail.com>
2024-08-25 16:16:25 -0300

    att

--- js.js
+++ js.js
@@ -1,3 +1,6 @@
 window.alert('Oi Olhoes, queria te dizer que estou alguns meses preparando esse presente pra você. Espero que goste!')
 
 window.alert('Aqui eu carrego o nosso PASSADO, por todo o nosso FUTURO! <3')
+
+var nome = window.prompt('o que eu mais ')
+window.alert(`nao é ${nome}`)
